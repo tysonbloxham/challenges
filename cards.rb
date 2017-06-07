c0c4aba78995615a920a2b39db1ea52639a02fcc
Tyson Bloxham <tysonsbloxham@gmail.com>
2017-06-07 10:31:16 -0600

    Added symbols to the card suits

--- cards.rb
+++ cards.rb
@@ -5,7 +5,7 @@
 # You need to be able to draw cards and discard them and have them not be in the deck anymore.
 
 
-@cards = ["ace of hearts", "2 of hearts", "3 of hearts", "4 of hearts", "5 of hearts", "6 of hearts", "7 of hearts", "8 of hearts", "9 of hearts", "10 of hearts", "jack of hearts", "queen of hearts", "king of hearts", "ace of spades", "2 of spades", "3 of spades", "4 of spades", "5 of spades", "6 of spades", "7 of spades", "8 of spades", "9 of spades", "10 of spades", "jack of spades", "queen of spades", "king of spades", "ace of diamonds", "2 of diamonds", "3 of diamonds", "4 of diamonds", "5 of diamonds", "6 of diamonds", "7 of diamonds", "8 of diamonds", "9 of diamonds", "10 of diamonds", "jack of diamonds", "queen of diamonds", "king of diamonds", "ace of clubs", "2 of clubs", "3 of clubs", "4 of clubs", "5 of clubs", "6 of clubs", "7 of clubs", "8 of clubs", "9 of clubs", "10 of clubs", "jack of clubs", "queen of clubs", "king of clubs"]
+@cards = ["ace of ♥️", "2 of ♥️", "3 of ♥️", "4 of ♥️", "5 of ♥️", "6 of ♥️", "7 of ♥️", "8 of ♥️", "9 of ♥️", "10 of ♥️", "jack of ♥️", "queen of ♥️", "king of ♥️", "ace of ♠️", "2 of ♠️", "3 of ♠️", "4 of ♠️", "5 of ♠️", "6 of ♠️", "7 of ♠️", "8 of ♠️", "9 of ♠️", "10 of ♠️", "jack of ♠️", "queen of ♠️", "king of ♠️", "ace of ♦️", "2 of ♦️", "3 of ♦️", "4 of ♦️", "5 of ♦️", "6 of ♦️", "7 of ♦️", "8 of ♦️", "9 of ♦️", "10 of ♦️", "jack of ♦️", "queen of ♦️", "king of ♦️", "ace of ♣️", "2 of ♣️", "3 of ♣️", "4 of ♣️", "5 of ♣️", "6 of ♣️", "7 of ♣️", "8 of ♣️", "9 of ♣️", "10 of ♣️", "jack of ♣️", "queen of ♣️", "king of ♣️"]
 
 def draw
 	puts "Type D to draw a card, or type S to stop."
