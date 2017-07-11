8ee127cec9dfca539a409d9e07765d714ed6530e
Tyson Bloxham <tysonsbloxham@gmail.com>
2017-07-11 03:23:33 -0600

    Initial commit

---
+++ rspec/nestedArray.rb
@@ -0,0 +1,23 @@
+require 'rspec'
+
+def find_element collection, element
+  collection.rassoc(element)
+  
+  # manual method
+  # collection.each do |c|
+  #   return c if c[1] == element
+end
+
+describe 'Find Element' do
+  it 'returns an array from a nested array if the second element equals the queried element' do
+    players = [
+      [27, 'Jose Altuve'],
+      [2,  'Alex Bregman'],
+      [1,  'Carlos Correa'],
+      [9,  'Marwin Gonzalez'],
+      [10, 'Yulieski Gurriel']
+    ]
+
+    expect(find_element(players, 'Jose Altuve')).to eq([27, 'Jose Altuve'])
+  end
+end
