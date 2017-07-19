52bd5e32e8aa7336aaaed30a630857636e919f30
Tyson Bloxham <tysonsbloxham@gmail.com>
2017-07-19 09:58:06 -0600

    Initial commit

---
+++ MadLib/madWords.rb
@@ -0,0 +1,43 @@
+# color	
+# superlative (ending is "est")	
+# adjective	
+# body part (plural)	
+# body part	
+# noun	
+# animal (plural)	
+# adjective	
+# adjective	
+# adjective
+
+def getting_words
+		
+	puts "Please write a color:"
+	@color = gets.chomp
+
+	puts "Please write a superlative ending in 'est':"
+	@superlative = gets.chomp
+
+	puts "Please write an adjective:"
+	@adjective = gets.chomp
+
+	puts "Please write a body part (plural):"
+	@body_parts = gets.chomp
+
+	puts "Please write a body part:"
+	@body_part = gets.chomp
+
+	puts "Please write a noun:"
+	@noun = gets.chomp
+
+	puts "Please write an animal:"
+	@animals = gets.chomp
+
+	puts "Please write an adjective:"
+	@adjective2 = gets.chomp
+
+	puts "Please write an adjective:"
+	@adjective3 = gets.chomp
+
+	puts "Please write an adjective:"
+	@adjective4 = gets.chomp
+end
---
+++ MadLib/madlib.rb
@@ -0,0 +1,14 @@
+# Level One: Write a mad lib that pulls from a separate file that has a list of nouns, verbs, adjectives, pronouns, etc.
+
+# Level Two: Get user input that writes into those files from your main file.
+
+# Level Two Plus: make them all in different files, making them as module. Figure out how to bring them into your main Mad Lib program.
+
+
+require './madWords.rb'
+
+getting_words
+
+story = "The #{@color} Dragon is the #{@superlative} Dragon of all. It has #{@adjective} #{@body_parts}, and a #{@body_part} shaped like a #{@noun}. It loves to eat #{@animals}, although it will feast on nearly anything. It is #{@adjective2} and #{@adjective3}. You must be #{@adjective4} around it, or you may end up as it`s meal!"
+
+puts story
