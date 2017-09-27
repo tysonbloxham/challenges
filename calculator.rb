38701866a87aba7c640fb9fa88e9e8b1115b5ae0
Tyson Bloxham <tysonsbloxham@gmail.com>
2017-09-26 22:12:20 -0600

    Refactor to add in solid methods

--- calculator.rb
+++ calculator.rb
@@ -4,17 +4,36 @@
 
 # Add or multiply all the numbers from one up to the other.
 
-puts "Please provide a number between 1 and 25."
-target = gets.chomp.to_i
-
-puts "Would you like to add or multiply?"
-action = gets.chomp.downcase.strip
-
-case action
-when "add"
-	final = (1..target).inject(:+)
-when "multiply"
-	final = (1..target).inject(:*)
+def init
+	puts "Please provide a number between 1 and 25"
+	target = gets.chomp.to_i
+	if target <= 25 && target >= 0
+		calc(target)
+	else
+		puts "That's not a valid input, try again."
+		init
+	end
 end
 
-puts final
+def calc(target)
+	puts "Would you like to add or multiply?"
+	action = gets.chomp.downcase.strip
+	case action
+	when "add"
+		add(target)
+	when "multiply"
+		multiply(target)		
+	end
+end
+
+def add(limit)
+	final = (1..limit).inject(:+)
+	puts final	
+end
+
+def multiply(limit)
+	final = (1..limit).inject(:*)
+	puts final	
+end
+
+init
