0ad33ebb1935d07edd0da94041b45ead2c53b24c
Tyson Bloxham <tysonsbloxham@gmail.com>
2017-09-26 22:00:52 -0600

    Refactor to seperate the guessing stage into new method

--- binaryComp.rb
+++ binaryComp.rb
@@ -5,11 +5,32 @@
 # Inform cpu if number is higher or lower
 # Change the range accordingly, get a new guess
 # 	-repeat until answer is given
-def initialize
+
+def init
+	system "clear"
 	@range = [*1..25]
 	puts "Choose a number between 1 and 25"
 	sleep(2)
 	game
+end
+
+def guessing(acc, guess)
+	if acc.downcase.slice(0) == 'h'
+		@range = @range.drop_while { |x| x < guess+1 }
+		print @range
+		puts
+		game
+	elsif acc.downcase.slice(0) == 'l'
+		@range.pop until @range.max == guess-1
+		print @range
+		puts
+		game
+	elsif acc.downcase.slice(0) == 'y'
+		puts "computer wins with a guess of #{guess}!"
+	else
+		puts "That's not a valid input, try again"
+		guessing(acc)
+	end		
 end
 
 def game
@@ -19,20 +40,8 @@ def game
 
 	puts "Should it guess higher or lower? Or type 'You win' if it guessed it."
 	accuracy = gets.chomp
+	guessing(accuracy, guess)
 
-	if accuracy == 'higher'
-		@range = @range.drop_while { |x| x < guess+1 }
-		print @range
-		puts
-		game
-	elsif accuracy == 'lower'
-		@range.pop until @range.max == guess-1
-		print @range
-		puts
-		game
-	else
-		puts "computer wins with a guess of #{guess}!"
-	end		
 end
 
-initialize
+init
